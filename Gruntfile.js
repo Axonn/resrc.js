601ee32ec4d45a79cff009193b184f3d329badb5
Janaka A <janaka@axonn.co.uk>
2015-01-13 12:56:11 +0000

    Update Grunt config to deploy to production

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,26 @@
+'use strict';
 module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+  var test, s3folderPrefix, pkg, folderPath, aws;
+  pkg = grunt.file.readJSON('package.json');
+  test = grunt.option('test') || false;
+    if (test) {
+        folderPath = 'resrcjs/test/'
+    } else {
+        folderPath = 'resrcjs/'
+    }
+    aws = grunt.file.readJSON('aws-keys.json');
+	
+  
+  
   grunt.initConfig({
-    pkg : grunt.file.readJSON('package.json'),
-    version : '0.9.3',
+	clean: ['.dist', '.tmp'],	
+    pkg : pkg,
+    version : pkg.version,
     srcPath : 'src/resrc.js',
     distPath : 'dist/resrc-<%= version %>.min.js',
+	
+	
     jshint : {
       options : {
         jshintrc : ".jshintrc"
@@ -29,10 +46,46 @@ module.exports = function (grunt) {
         files : ['<%= srcPath %>'],
         tasks : ['jshint', 'uglify']
       }
-    }
+	  
+    },
+	release: {
+        options: {
+             npm: false
+        }
+    },
+	aws_s3: {
+		options: {
+			accessKeyId: aws.AWSAccessKeyId,
+			secretAccessKey: aws.AWSSecretKey,
+			bucket: 'agoraa-global-js-libraries',
+			region: 'eu-west-1',
+			params: {
+				// 30 days (60 * 60 * 24 * 30)
+				CacheControl: 'public, max-age=2592000',
+				Expires: new Date(Date.now() + 2592000 * 1000)
+			}
+		},
+		dist: {
+			files: [{
+					params: {
+						ContentType: 'application/javascript',
+						ContentEncoding: 'gzip',
+					},
+					expand: true,
+					cwd: '.dist/',
+					src: '<%= distPath %>',
+					dest: folderPath
+				}
+			]
+		}
+	}	
   });
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('build', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['clean', 'jshint', 'uglify']);
+  grunt.registerTask('push', ['build', 'aws_s3']);
+  grunt.loadNpmTasks('grunt-release');
 };
